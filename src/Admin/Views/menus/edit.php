2461fc9def28699f4d4f41c94a7bf1419f1b4d60
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2013-12-31 10:53:14 -0500

    draft of the changes to the admin-side menu edit form

--- src/Admin/Views/menus/edit.php
+++ src/Admin/Views/menus/edit.php
@@ -44,6 +44,9 @@ jQuery(document).ready(function(){
                 <li class="active">
                     <a href="#tab-basics" data-toggle="tab"> Basics </a>
                 </li>
+                <li>
+                    <a href="#tab-display" data-toggle="tab"> Display </a>
+                </li>
                 <?php foreach ((array) $this->event->getArgument('tabs') as $key => $title ) { ?>
                 <li>
                     <a href="#tab-<?php echo $key; ?>" data-toggle="tab"> <?php echo $title; ?> </a>
@@ -64,7 +67,7 @@ jQuery(document).ready(function(){
                     <!-- /.form-group -->
 
                     <div class="form-group">
-                        <label for="slug">URL</label>
+                        <label>URL</label>
                         <input type="text" name="details[url]" placeholder="URL" value="<?php echo $flash->old('details.url'); ?>" class="form-control" />
                     </div>
                     <!-- /.form-group -->
@@ -131,6 +134,88 @@ jQuery(document).ready(function(){
                     <!-- /.form-group -->
                     <?php } ?>
                 
+                </div>
+                
+                <div class="tab-pane" id="tab-display">
+                
+                    <div class="form-group">
+                        <label>Display Title</label>
+                        <div class="form-group">
+                        <label class="radio-inline">
+                            <input type="radio" name="display_title" value="1" <?php if (is_null($flash->old('display_title')) || $flash->old('display_title') == '1') { echo 'checked'; } ?>> Yes
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="display_title" value="0" <?php if ($flash->old('display_title') == '0') { echo 'checked'; } ?>> No
+                        </label>
+                        </div>
+                    </div>
+                    <!-- /.form-group -->
+                    
+                    <div class="form-group">
+                        <label>Display Type</label>
+                        <div class="form-group">
+                        <label class="radio-inline">
+                            <input type="radio" name="display_type" value="megamenu" <?php if ($flash->old('display_type') == 'megamenu') { echo 'checked'; } ?>> Megamenu
+                        </label>
+                        <label class="radio-inline">
+                            <input type="radio" name="display_type" value="dropdown" <?php if (!$flash->old('display_type') || $flash->old('display_type') == 'dropdown') { echo 'checked'; } ?>> Dropdown
+                        </label>
+                        </div>
+                    </div>
+                    <!-- /.form-group -->
+                    
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <label>Megamenu Options</label>
+                            <p class="help-block"><small>These options only apply 1) when the 'Display Type' above is set to "megamenu" and 2) if this is a first-level menu item</small></p>
+                        </div>
+                        <div class="panel-body">
+                        
+                            <div class="form-group row">
+                                <label class="col-md-2">Width</label>
+                                <div class="col-md-2">
+                                <select name="megamenu[width]" class="form-control">
+                                    <option value="25" <?php if ($flash->old('megamenu.width') == 25 || is_null($flash->old('megamenu.width')) ) { echo "selected='selected'"; } ?>>25% of the entire menu</option>
+                                    <option value="50" <?php if ($flash->old('megamenu.width') == 50) { echo "selected='selected'"; } ?>>50%</option>
+                                    <option value="100" <?php if ($flash->old('megamenu.width') == 100) { echo "selected='selected'"; } ?>>100%</option>
+                                </select>
+                                <p class="help-block">This is the width of the <i>entire</i> Megamenu.  This option only applies to first-level menu items.
+                                </div>
+                            </div>
+                            <!-- /.form-group -->
+                        
+                            <div class="form-group row">
+                                <label class="col-md-2">Columns</label>
+                                <div class="col-md-2">
+                                <select name="megamenu[columns]" class="form-control">
+                                    <option value="1" <?php if ($flash->old('megamenu.columns') == 1) { echo "selected='selected'"; } ?>>1</option>
+                                    <option value="2" <?php if ($flash->old('megamenu.columns') == 2) { echo "selected='selected'"; } ?>>2</option>
+                                    <option value="3" <?php if ($flash->old('megamenu.columns') == 3) { echo "selected='selected'"; } ?>>3</option>
+                                    <option value="4" <?php if ($flash->old('megamenu.columns') == 4) { echo "selected='selected'"; } ?>>4</option>
+                                    <option value="5" <?php if ($flash->old('megamenu.columns') == 5) { echo "selected='selected'"; } ?>>5</option>
+                                    <option value="6" <?php if ($flash->old('megamenu.columns') == 6) { echo "selected='selected'"; } ?>>6</option>
+                                </select>
+                                </div>
+                            </div>
+                            <!-- /.form-group -->
+                            
+                            <div class="form-group row">
+                                <label class="col-md-2">Group Child Items</label>
+                                <div class="form-group col-md-10">
+                                <label class="radio-inline">
+                                    <input type="radio" name="megamenu[group_children]" value="1" <?php if (is_null($flash->old('megamenu.group_children')) || $flash->old('megamenu.group_children') == '1') { echo 'checked'; } ?>> Yes
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="megamenu[group_children]" value="0" <?php if ($flash->old('megamenu.group_children') == '0') { echo 'checked'; } ?>> No
+                                </label>
+                                <p class="help-block">If set to YES, this item's children will be displayed in a grouped list with their children. 
+                                </div>
+                            </div>
+                            <!-- /.form-group -->
+                            
+                        </div>
+                    </div>
+                    
                 </div>
                 
                 <?php foreach ((array) $this->event->getArgument('content') as $key => $content ) { ?>
