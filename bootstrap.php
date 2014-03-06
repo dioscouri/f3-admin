1d5b2d9c8db57ba23d9c6a62ddc7131d6f6feaf5
Lukas Polak <lpolak@dioscouri.com>
2014-03-06 00:38:53 -0500

    Bootstrapping v2

--- bootstrap.php
+++ bootstrap.php
@@ -1,30 +1,17 @@
 <?php 
-$f3 = \Base::instance();
-$global_app_name = $f3->get('APP_NAME');
 
-switch ($global_app_name) 
-{
-    case "admin":
-        // TODO if the assets haven't ben copied to the public folder, do it
-        
-    	// register all the routes
-    	\Dsc\System::instance()->get('router')->mount( new \Admin\Routes, 'admin' );
-    	 
-        // new way
-        \Dsc\System::instance()->get('theme')->setTheme('AdminTheme', $f3->get('PATH_ROOT') . 'vendor/dioscouri/f3-admin/src/Admin/Theme/' );
-        \Dsc\System::instance()->get('theme')->registerViewPath( $f3->get('PATH_ROOT') . 'vendor/dioscouri/f3-admin/src/Admin/Views/', 'Admin/Views' );
-        
-        // append this app's UI folder to the path
-        $ui = $f3->get('UI');
-        $ui .= ";" . $f3->get('PATH_ROOT') . "vendor/dioscouri/f3-admin/src/Admin/Views/";
-        $f3->set('UI', $ui);
-        
-        // append this app's template folder to the path
-        $templates = $f3->get('TEMPLATES');
-        $templates .= ";" . $f3->get('PATH_ROOT') . "vendor/dioscouri/f3-admin/src/Admin/Templates/";
-        $f3->set('TEMPLATES', $templates);
-        
-        
-        break;
+class AdminBootstrap extends \Dsc\BaseBootstrap{
+	protected $dir = __DIR__;
+	protected $namespace = 'Admin';
+
+	protected function runAdmin(){
+		$f3 = \Base::instance();
+		\Dsc\System::instance()->get('theme')->setTheme('AdminTheme',$this->dir . '/src/Admin/Theme/' );
+		// append this app's template folder to the path
+		$templates = $f3->get('TEMPLATES');
+		$templates .= ";" . $this->dir . "/src/Admin/Templates/";
+		$f3->set('TEMPLATES', $templates);
+		parent::runAdmin();
+	}
 }
-?>
+$app = new AdminBootstrap();
