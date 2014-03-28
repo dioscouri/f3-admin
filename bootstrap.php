02eb7036755fca0f1d2d7b6cc8d5f00407856eb8
Chris French <chris@ammonitenetworks.com>
2014-03-28 11:14:01 -0600

    adding auto symlinking

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,15 @@ class AdminBootstrap extends \Dsc\Bootstrap{
 	protected $namespace = 'Admin';
 
 	protected function runAdmin(){
+
 		$f3 = \Base::instance();
+		//link the theme to public folder
+		if(!is_dir($f3->get('PATH_ROOT').'public/AdminTheme')) {
+			$publictheme = $f3->get('PATH_ROOT').'public/AdminTheme';
+			$admintheme = $f3->get('PATH_ROOT').'vendor/dioscouri/f3-admin/AdminTheme';
+			exec('ln -s '. $admintheme . ' '. $publictheme);
+		}
+		
 		\Dsc\System::instance()->get('theme')->setTheme('AdminTheme',$this->dir . '/src/Admin/Theme/' );
 		// append this app's template folder to the path
 		$templates = $f3->get('TEMPLATES');
