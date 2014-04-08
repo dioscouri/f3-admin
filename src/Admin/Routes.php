5404af23cf15f2dea4e49daa39f381bb62ba9faa
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-07 22:25:20 -0400

    minor change to route formatting

--- src/Admin/Routes.php
+++ src/Admin/Routes.php
@@ -1,84 +1,84 @@
 <?php
-
 namespace Admin;
 
 /**
  * Group class is used to keep track of a group of routes with similar aspects (the same controller, the same f3-app and etc)
  */
-class Routes extends \Dsc\Routes\Group{
-	
-	
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Initializes all routes for this group
-	 * NOTE: This method should be overriden by every group
-	 */
-	public function initialize(){
-		$this->setDefaults(
-				array(
-					'namespace' => '\Admin\Controllers',
-					'url_prefix' => '/admin'
-				)
-		);
-		
-		$this->add( '', 'GET', array(
-								'controller' => 'Home',
-								'action' => 'display'
-								));
+class Routes extends \Dsc\Routes\Group
+{
 
-		$this->add( '/login', 'GET', array(
-				'controller' => 'Login',
-				'action' => 'login'
-		));
-
-		$this->add( '/login', 'POST', array(
-				'controller' => 'Login',
-				'action' => 'auth'
-		));
-
-		$this->add( '/logout', 'GET', array(
-				'controller' => 'Login',
-				'action' => 'logout'
-		));
-
-		$this->add( '/system/@task', 'GET', array(
-				'controller' => 'System',
-				'action' => '@task'
-		));
-
-		$this->addSettingsRoutes();
-		$this->addCrudList( 'Logs' );
-		$this->addCrudList( 'Queue' );
-		$this->addCrudList( 'Menus' );
-		
-		$this->add( '/menus/all', 'GET', array(
-				'controller' => 'Menus',
-				'action' => 'getAll',
-				'ajax' => true
-		));
-
-		$this->add( '/menus/@id', array('GET', 'POST'), array(
-				'controller' => 'Menus',
-				'action' => 'index'
-		));
-
-		$this->add( '/menus/@id/page/@page', array('GET', 'POST'), array(
-				'controller' => 'Menus',
-				'action' => 'index'
-		));
-		
-		$this->addCrudItem( 'Menu' );
-		$this->add( '/menu/moveup/@id', 'GET', array(
-				'controller' => 'Menu',
-				'action' => 'moveUp'
-		));
-
-		$this->add( '/menu/movedown/@id', 'GET', array(
-				'controller' => 'Menu',
-				'action' => 'moveDown'
-		));
-	}
+    /**
+     * Initializes all routes for this group
+     * NOTE: This method should be overriden by every group
+     */
+    public function initialize()
+    {
+        $this->setDefaults( array(
+            'namespace' => '\Admin\Controllers',
+            'url_prefix' => '/admin' 
+        ) );
+        
+        $this->add( '', 'GET', array(
+            'controller' => 'Home',
+            'action' => 'display' 
+        ) );
+        
+        $this->add( '/login', 'GET', array(
+            'controller' => 'Login',
+            'action' => 'login' 
+        ) );
+        
+        $this->add( '/login', 'POST', array(
+            'controller' => 'Login',
+            'action' => 'auth' 
+        ) );
+        
+        $this->add( '/logout', 'GET', array(
+            'controller' => 'Login',
+            'action' => 'logout' 
+        ) );
+        
+        $this->add( '/system/@task', 'GET', array(
+            'controller' => 'System',
+            'action' => '@task' 
+        ) );
+        
+        $this->addSettingsRoutes();
+        $this->addCrudList( 'Logs' );
+        $this->addCrudList( 'Queue' );
+        $this->addCrudList( 'Menus' );
+        
+        $this->add( '/menus/all', 'GET', array(
+            'controller' => 'Menus',
+            'action' => 'getAll',
+            'ajax' => true 
+        ) );
+        
+        $this->add( '/menus/@id', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Menus',
+            'action' => 'index' 
+        ) );
+        
+        $this->add( '/menus/@id/page/@page', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Menus',
+            'action' => 'index' 
+        ) );
+        
+        $this->addCrudItem( 'Menu' );
+        $this->add( '/menu/moveup/@id', 'GET', array(
+            'controller' => 'Menu',
+            'action' => 'moveUp' 
+        ) );
+        
+        $this->add( '/menu/movedown/@id', 'GET', array(
+            'controller' => 'Menu',
+            'action' => 'moveDown' 
+        ) );
+    }
 }
