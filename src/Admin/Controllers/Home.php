e16179b32d09ce12cc011da450571ef442fd1d19
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-22 12:32:02 -0400

    removed acl checker from the home view

--- src/Admin/Controllers/Home.php
+++ src/Admin/Controllers/Home.php
@@ -5,8 +5,6 @@ class Home extends BaseAuth
 {
     public function display()
     {
-        $this->checkAccess( __CLASS__, __FUNCTION__ );
-        
         echo \Dsc\System::instance()->get('theme')->renderTheme('Admin/Views::home/default.php');
     }
 }
