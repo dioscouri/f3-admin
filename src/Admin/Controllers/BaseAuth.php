c9231d7ff3c211117b5ed5fafa0fcb788b5dd2f7
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-23 12:03:51 -0400

    made baseAuth use new requireIdentity method

--- src/Admin/Controllers/BaseAuth.php
+++ src/Admin/Controllers/BaseAuth.php
@@ -3,16 +3,9 @@ namespace Admin\Controllers;
 
 class BaseAuth extends Base 
 {
-    public function beforeRoute($f3)
+    public function beforeRoute()
     {
-        $identity = $this->getIdentity();
-        if (empty($identity->id))
-        {
-            $path = $this->inputfilter->clean( $f3->hive()['PATH'], 'string' );
-            \Dsc\System::instance()->get( 'session' )->set( 'admin.login.redirect', $path );
-
-            $f3->reroute('/admin/login');
-        }
+        $this->requireIdentity();
     }    
 }
 ?>
