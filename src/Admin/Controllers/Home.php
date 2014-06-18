8dd628a24b0f291943e4fbb1479b28ff891a147b
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-18 14:24:32 -0400

    made admin menu build automatically on first login, fixes #40

--- src/Admin/Controllers/Home.php
+++ src/Admin/Controllers/Home.php
@@ -3,6 +3,19 @@ namespace Admin\Controllers;
 
 class Home extends BaseAuth 
 {
+    public function beforeRoute()
+    {
+        parent::beforeRoute();
+        
+        $settings = \Admin\Models\Settings::fetch();
+        
+        if (empty($settings->admin_menu_id)) 
+        {
+             $this->session->set('rebuild-menu.redirect', '/admin');
+             return $this->app->reroute( '/admin/system/rebuildAdminMenu' );
+        }
+    }
+        
     public function display()
     {
         $this->app->set('meta.title', 'Home');
--- src/Admin/Models/Settings.php
+++ src/Admin/Models/Settings.php
@@ -1,33 +1,36 @@
-<?php 
+<?php
 namespace Admin\Models;
 
 class Settings extends \Dsc\Mongo\Collections\Settings
 {
-	protected $__type = 'admin.settings';
 
-	public $admin_menu_id = null;
-	
-	public $system = array(
-		'force_ssl' => 0,
-	    'page_title_suffix' => null,
-	);
+    protected $__type = 'admin.settings';
 
-	public $integration = array(
-			'kissmetrics' => array(
-					'enabled' => 0,
-					'key' => '',
-			),
-	);
-	
-	public function enabledIntegration( $name ){
-		$result = false;
-	
-		switch( $name ){
-			case 'kissmetrics' :
-				$result = $this->{'integration.kissmetrics.enabled'} && strlen( $this->{'integration.kissmetrics.key'} );
-				break;
-		}
-	
-		return $result;
-	}
+    public $admin_menu_id = null;
+
+    public $system = array(
+        'force_ssl' => 0,
+        'page_title_suffix' => null
+    );
+
+    public $integration = array(
+        'kissmetrics' => array(
+            'enabled' => 0,
+            'key' => ''
+        )
+    );
+
+    public function enabledIntegration($name)
+    {
+        $result = false;
+        
+        switch ($name)
+        {
+            case 'kissmetrics':
+                $result = $this->{'integration.kissmetrics.enabled'} && strlen($this->{'integration.kissmetrics.key'});
+                break;
+        }
+        
+        return $result;
+    }
 }
