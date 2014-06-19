8c5319eec366bee130d7cb38d0a2a42c8994301b
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-19 14:47:45 +0000

    removed gray bg from login screen

--- src/Admin/Theme/login.php
+++ src/Admin/Theme/login.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html class="login no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="login no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="login no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="login no-js"> <!--<![endif]-->
 
 <head>
     <?php echo $this->renderView('Admin/Views::head.php'); ?>
