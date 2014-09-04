901ad7cb4658b1687039702b7f1b52e977cd957a
Chris French <chris@ammonitenetworks.com>
2014-09-04 10:10:21 -0600

    Update BaseAuth.php
    Adding a hack to force admin only in admin

--- src/Admin/Controllers/BaseAuth.php
+++ src/Admin/Controllers/BaseAuth.php
@@ -6,6 +6,14 @@ class BaseAuth extends Base
     public function beforeRoute()
     {
     	parent::beforeRoute();
+    	//TODO remove this hack, after ACL is finished
+    	$user = $this->auth->getIdentity();
+    	if($user->role != 'root') {
+    		$this->auth->logout();
+    		\Dsc\System::addMessage('Not Authorized');
+    		$this->app->reroute('/admin/login');
+    	}
+    	
         $this->requireIdentity();
     }    
 }
