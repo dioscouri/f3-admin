97cff343ea695492bd92adf4d8615e90d14a3f5a
Building Berms Server <chris@ammonitenetworks.com>
2014-11-11 23:10:18 +0000

    small bug fix in auth roles hacks

--- src/Admin/Controllers/BaseAuth.php
+++ src/Admin/Controllers/BaseAuth.php
@@ -6,19 +6,28 @@ class BaseAuth extends Base
     public function beforeRoute()
     {
     	parent::beforeRoute();
-    	//TODO remove this hack, after ACL is finished
-    	$role = $this->auth->getIdentity()->getRole();
-    	 //TODO maybe the Role gets stored in the session to avoid one more DB query every load, or maybe none of this makes it to the future
-    	if($role->slug == 'root') {
-    		//root always has access no farther checks needed
-    	} elseif(!$role->adminaccess){
-    		//if this role is not admin and not given admin permissions
-    		$this->auth->logout();
-    		\Dsc\System::addMessage('Not Authorized');
-    		$this->app->reroute('/admin/login');
-    	}
-    	
-        $this->requireIdentity();
+
+    	$this->requireIdentity();
+        
+        //TODO remove this hack, after ACL is finished
+        $user = $this->auth->getIdentity();
+        //TODO maybe the Role gets stored in the session to avoid one more DB query every load, or maybe none of this makes it to the future
+        if($user->id == $this->app->get('safemode.id')) {
+        	return;
+        }
+         
+         
+        $role = $user->getRole();
+         
+        if($role->slug == 'root') {
+        	//root always has access no farther checks needed
+        } elseif(!$role->adminaccess){
+        	//if this role is not admin and not given admin permissions
+        	$this->auth->logout();
+        	\Dsc\System::addMessage('Not Authorized');
+        	$this->app->reroute('/admin/login');
+        }
+         
     }    
 }
 ?>
