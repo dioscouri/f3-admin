c0687d2fff74036e74493f6a65841f2bb9562454
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-11-12 12:27:11 +0000

    require identity must happen before you get the identity role, fixes fatal error when session expires

--- src/Admin/Controllers/BaseAuth.php
+++ src/Admin/Controllers/BaseAuth.php
@@ -6,19 +6,30 @@ class BaseAuth extends Base
     public function beforeRoute()
     {
     	parent::beforeRoute();
+    	
+    	$this->requireIdentity();
+    	
     	//TODO remove this hack, after ACL is finished
     	$role = $this->auth->getIdentity()->getRole();
+    	
+    	if (empty($role->slug)) 
+    	{
+    	    $this->auth->logout();
+    	    \Dsc\System::addMessage('Not Authorized');
+    	    $this->app->reroute('/admin/login');    	    
+    	}
+    	
     	 //TODO maybe the Role gets stored in the session to avoid one more DB query every load, or maybe none of this makes it to the future
-    	if($role->slug == 'root') {
+    	if ($role->slug == 'root') {
     		//root always has access no farther checks needed
-    	} elseif(!$role->adminaccess){
+    	} 
+    	elseif (empty($role->adminaccess))
+    	{
     		//if this role is not admin and not given admin permissions
     		$this->auth->logout();
     		\Dsc\System::addMessage('Not Authorized');
     		$this->app->reroute('/admin/login');
     	}
-    	
-        $this->requireIdentity();
     }    
 }
 ?>
