035c074edd9a36fe7c6b5551420f2aa4d53ea46a
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2015-04-09 11:39:32 +0000

    added menu item to header when editing

--- src/Admin/Views/menus/edit.php
+++ src/Admin/Views/menus/edit.php
@@ -9,6 +9,20 @@ jQuery(document).ready(function(){
 });
 </script>
 
+<div class="row">
+	<div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
+		<h1 class="page-title txt-color-blueDark">
+			<i class="fa fa-table fa-fw "></i> 
+				<a href="./admin/menus/<?php echo $item->tree; ?>">Menus</a>
+				<span> > <?php echo $flash->old('title'); ?></span>
+		</h1>
+	</div>
+	<div class="col-xs-12 col-sm-5 col-md-5 col-lg-8">
+
+	</div>
+</div>
+
+
 <div class="well">
 
 <form id="detail-form" class="form" method="post">
